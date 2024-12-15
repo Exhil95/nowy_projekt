74e96c99819bdf31a1d2e47c942b555aaf6e8e4c
Exhil95 <d.kuczka@gmail.com>
2024-12-15 15:15:50 +0100

    serializacja danych

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from kontroler.kontroler import KontrolerSzkalrni
 from util.api_klient import PogodaAPI
 from util.api_key import API_KEY
 from enumy.lokacje import Lokalizacje
+from util.menago import Manager
+from util.przetwarzanie import ObslugaDanych 
   
 LOCATION = Lokalizacje.algieria
 
@@ -10,14 +12,13 @@ if __name__ == "__main__":
     szklarnia = Szklarnia(nazwa="Szklarnia1")
     kontroler = KontrolerSzkalrni(szklarnia)
     api_klient = PogodaAPI(API_KEY)
+    manager = Manager()
+    obsluga = ObslugaDanych(api_klient, manager) 
 
-   # try:
-    for miasta in Lokalizacje:
-        print(f"Sprawdzanie danych w {miasta.name}")
-        czujnik = api_klient.sprawdz_pogode(miasta.value)
-        print(f"Informacje Pogodowe: {czujnik}")
 
-        kontroler.zmien_warunki(czujnik)
-        print(szklarnia)
-    #except (ValueError, ConnectionError) as e:
-    #    print(f"Error: {e}")
+    dane_pogodowe = ObslugaDanych.zdobadz_dane_pogodowe()
+    deserializowane_dane = ObslugaDanych.deserializuj_dane_pogodowe(dane_pogodowe)
+    for lokation_name, dane_czujnik in deserializowane_dane.items():
+       print(f"Przetwarzanie danych pogodowych dla lokalizacji: {lokation_name}")
+       kontroler.zmien_warunki(dane_czujnik)
+       print(szklarnia)
