8d83e1c8795c81bc3cb185deaf9ad37f8addce7e
Exhil95 <d.kuczka@gmail.com>
2024-12-15 11:33:55 +0100

    dodanie enuma

---
+++ enumy/warunki_pogodowe.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+class WarunkiPogodowe(Enum):
+    SUNNY = "słonecznie"
+    CLOUDY = "pochmurnie"
+    RAINY = "deszczowo"
+    SNOWY = "śnieżnie"
+    WINDY = "wietrznie"
+    PARTLY_CLOUDY = "częściowe zachmurzenie"
