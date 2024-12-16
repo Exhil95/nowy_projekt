af2411519f6afa3e183d5bcbdcea3778297c4300
Exhil95 <d.kuczka@gmail.com>
2024-12-16 14:25:53 +0100

    testowanie class Baza

--- util/dbhandler.py
+++ util/dbhandler.py
@@ -7,19 +7,40 @@ class Baza:
         self.plik = self._tworz_nazwe_pliku()
     
     def _tworz_nazwe_pliku(self) -> str:
-        dzis:str = datetime.now().strftime("%d-%m-%Y")
+        dzis: str = datetime.now().strftime("%d-%m-%Y")
         return dzis + ".db"
     
     def sprawdz_istnienie_db(self) -> bool:
         if os.path.isfile(self.plik):
-            print("baza istnieje.")
-            return False
+            print("Baza istnieje.")
+            return True
         else:
             with sql.connect(self.plik) as con:
-                print("stworzono bazę danych.")
-            return True
+                print("Stworzono bazę danych.")
+            return False
         
-    def polacz_baza(self) -> None:
-        if not self.sprawdz_istnienie_db():
-            raise FileNotFoundError(f"Baza danych '{self.plik}'nie istnieje, użyj metody sprawdz_istnienie_db()")
+    def polacz_baza(self):
+        if not os.path.isfile(self.plik):
+            raise FileNotFoundError(f"Baza danych '{self.plik}' nie istnieje. Użyj metody `sprawdz_istnienie_db()`.")
         return sql.connect(self.plik)
+
+def main():
+    baza = Baza()
+    baza.sprawdz_istnienie_db()
+    
+    polaczenie = None
+    
+    try:
+        polaczenie = baza.polacz_baza()
+        print("Połączono z bazą danych:", polaczenie)
+        
+    except FileNotFoundError as e:
+        print("Błąd:", e)
+        
+    finally:
+        if polaczenie:
+            polaczenie.close()
+            print("Połączenie zostało zamknięte.")
+
+if __name__ == "__main__":
+    main()
