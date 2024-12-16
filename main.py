fb9f593386215d288f95523df3935fceaef4b87c
Exhil95 <d.kuczka@gmail.com>
2024-12-16 17:28:33 +0100

    `Added new weather conditions to enumy/warunki_pogodowe.py and updated main.py to use them. Removed 16-12-2024.db file.`

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from util.api_key import API_KEY
 from enumy.lokacje import Lokalizacje
 import time
 from util.dbhandler import Baza
+from modele.czujnik_szklarnia import CzujnikDane
+from enumy.warunki_pogodowe import WarunkiPogodowe
 
 def main() -> None:
     start = time.perf_counter()
@@ -34,20 +36,26 @@ def test_main() -> None:
     kontroler = KontrolerSzkalrni(szklarnia)
     api_klient = PogodaAPI(API_KEY)
     baza = Baza()
+   
     
     if not baza.sprawdz_istnienie_db():
         baza.utworz_tabele()
         for miasta in Lokalizacje:
             print(f"Sprawdzanie danych w {miasta.name}")
-            czujnik = api_klient.sprawdz_pogode(miasta.value)
-            baza.dodaj_dane(miasta.name, czujnik.temperatura, czujnik.wilgotnosc, czujnik.warunki.name)
+            czujnik = api_klient.sprawdz_pogode(miasta.name)
+            baza.dodaj_dane(miasta.name, czujnik.temperatura, czujnik.wilgotnosc, czujnik.warunki.value)
             print(f"Informacje Pogodowe: {czujnik}")
             kontroler.zmien_warunki(czujnik)
             print(szklarnia)
     else:
         dane_baza:list = baza.pobierz_dane()
         for i in dane_baza:
-            print(f"Sprawdzanie danych w {i[0]}")
+            print(f"Sprawdzanie danych z bazy w {i[0]}")
+            a = WarunkiPogodowe(i[3])
+            czujnik = CzujnikDane(temperatura=i[1], wilgotnosc=i[2], warunki=a)
+            print(f"Informacje Pogodowe: {czujnik}")
+            kontroler.zmien_warunki(czujnik)
+            print(szklarnia)
             
     
     finish = time.perf_counter()
