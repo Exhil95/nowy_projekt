c5193dc3d74aa9a6d68ac700774904d2771a96fe
Exhil95 <d.kuczka@gmail.com>
2024-12-16 17:29:06 +0100

    `Reverted test_main() to call main() and restored original main() function`

--- main.py
+++ main.py
@@ -14,28 +14,6 @@ def main() -> None:
     kontroler = KontrolerSzkalrni(szklarnia)
     api_klient = PogodaAPI(API_KEY)
     baza = Baza()
-    baza.sprawdz_istnienie_db()
-    baza.utworz_tabele()
-
-    for miasta in Lokalizacje:
-        print(f"Sprawdzanie danych w {miasta.name}")
-        czujnik = api_klient.sprawdz_pogode(miasta.value)
-        baza.dodaj_dane(miasta.name, czujnik.temperatura, czujnik.wilgotnosc, czujnik.warunki.name)
-        print(f"Informacje Pogodowe: {czujnik}")
-        kontroler.zmien_warunki(czujnik)
-        print(szklarnia)
-        
-    finish = time.perf_counter()
-    wynik = round(finish - start, 2)
-    print(f"Zakończono w czasie {wynik} sekund(y)")
-
-
-def test_main() -> None:
-    start = time.perf_counter()
-    szklarnia = Szklarnia(nazwa="Szklarnia1")
-    kontroler = KontrolerSzkalrni(szklarnia)
-    api_klient = PogodaAPI(API_KEY)
-    baza = Baza()
    
     
     if not baza.sprawdz_istnienie_db():
@@ -63,5 +41,5 @@ def test_main() -> None:
     print(f"Zakończono w czasie {wynik} sekund(y)")
           
 if __name__ == "__main__":
-    # main()
-    test_main()
+    main()
+    
