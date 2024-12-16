d2f5a71e8ded7a7ca1c115f6c3342d32bf1b0f36
Exhil95 <d.kuczka@gmail.com>
2024-12-16 12:58:09 +0100

    `Refactor main.py: add timing, remove commented code, and restructure main function`

--- main.py
+++ main.py
@@ -3,22 +3,25 @@ from kontroler.kontroler import KontrolerSzkalrni
 from util.api_klient import PogodaAPI
 from util.api_key import API_KEY
 from enumy.lokacje import Lokalizacje
-from util.menago import Manager
-from util.przetwarzanie import ObslugaDanych 
-  
-LOCATION = Lokalizacje.algieria
+import time
 
-if __name__ == "__main__":
+def main() -> None:
+    start = time.perf_counter()
     szklarnia = Szklarnia(nazwa="Szklarnia1")
     kontroler = KontrolerSzkalrni(szklarnia)
     api_klient = PogodaAPI(API_KEY)
-    manager = Manager()
-    obsluga = ObslugaDanych(api_klient, manager) 
 
+    for miasta in Lokalizacje:
+        print(f"Sprawdzanie danych w {miasta.name}")
+        czujnik = api_klient.sprawdz_pogode(miasta.value)
+        print(f"Informacje Pogodowe: {czujnik}")
 
-    dane_pogodowe = ObslugaDanych.zdobadz_dane_pogodowe()
-    deserializowane_dane = ObslugaDanych.deserializuj_dane_pogodowe(dane_pogodowe)
-    for lokation_name, dane_czujnik in deserializowane_dane.items():
-       print(f"Przetwarzanie danych pogodowych dla lokalizacji: {lokation_name}")
-       kontroler.zmien_warunki(dane_czujnik)
-       print(szklarnia)
+        kontroler.zmien_warunki(czujnik)
+        print(szklarnia)
+        
+    finish = time.perf_counter()
+    wynik = round(finish - start, 2)
+    print(f"Zakończono w czasie {wynik} sekund")
+        
+if __name__ == "__main__":
+    main()
