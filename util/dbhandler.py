8cbb74ff12938d82f0c3f01fcbae0cd99b2acdba
Exhil95 <d.kuczka@gmail.com>
2024-12-16 13:52:47 +0100

    stworzenie claas Baza

--- util/dbhandler.py
+++ util/dbhandler.py
@@ -2,13 +2,24 @@ import sqlite3 as sql
 import os
 from datetime import datetime
 
-
-teraz = datetime.now()
-dzis = teraz.strftime("%d-%m-%Y")
-plik:str = dzis + ".db"
-
-if os.path.isfile(plik):
-    print("baza istnieje")
-else:
-    con = sql.connect(plik)
-    print("stworzono db0")
+class Baza:
+    def __init__(self) -> None:
+        self.plik = self._tworz_nazwe_pliku()
+    
+    def _tworz_nazwe_pliku(self) -> str:
+        dzis:str = datetime.now().strftime("%d-%m-%Y")
+        return dzis + ".db"
+    
+    def sprawdz_istnienie_db(self) -> bool:
+        if os.path.isfile(self.plik):
+            print("baza istnieje.")
+            return False
+        else:
+            with sql.connect(self.plik) as con:
+                print("stworzono bazę danych.")
+            return True
+        
+    def polacz_baza(self) -> None:
+        if not self.sprawdz_istnienie_db():
+            raise FileNotFoundError(f"Baza danych '{self.plik}'nie istnieje, użyj metody sprawdz_istnienie_db()")
+        return sql.connect(self.plik)
